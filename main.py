10b48a8cb4ce32a72c96cb8b2159ba3d88f86e48
Alexandre Carneiro <72991728+SDAlez@users.noreply.github.com>
2022-10-09 09:36:49 -0300

    Added connection to database

--- main.py
+++ main.py
@@ -1,120 +1,128 @@
-import json
+import requests
 from flask import Flask, request, jsonify
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = 'library_admin'
+app.config['MYSQL_DATABASE_PASSWORD'] = '1S1!#3iOO#i@'
+app.config['MYSQL_DATABASE_DB'] = 'library_database'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
-book_list = [
-    {
-        "id": 0,
-        "author": "Adichie",
-        "language": "English",
-        "title": "Purple Hibiscus"
-    },
-    {
-        "id": 1,
-        "author": "Sun Tzu",
-        "language": "Chinese",
-        "title": "Art of War"
-    },
-    {
-        "id": 2,
-        "author": "Machado de Assis",
-        "language": "Portuguese",
-        "title": "Don Casmurro"
-    }
-]
+conn = mysql.connect()
+cursor = conn.cursor()
 
 @app.route('/books', methods=['GET', 'POST'])
 def books():
     
-    
     if request.method == 'GET':
-        if(len(book_list) > 0):
-            return jsonify(book_list)
-        else:
-            'Nothing Found', 404
+        
+        SQL_QUERY = 'SELECT * FROM books'
+        
+        cursor.execute(SQL_QUERY)
+        book_list = []
+        
+        for row in cursor:
             
+            book = {
+                'id': row[0],
+                'title': row[1],
+                'author': row[2],
+                'language': row [3]
+            }
             
+            book_list.append(book)
+            
+        return jsonify(book_list), 200
             
     if request.method == 'POST':
         try:
             new_author = request.form['author']
             new_lang = request.form['language']
             new_title = request.form['title']
-            id = len(book_list)
-            
-            new_obj = {
-                'id': id,
-                'author': new_author,
-                'language': new_lang,
-                'title': new_title
-            }
-            
-            book_list.append(new_obj)
-            return jsonify(new_obj), 201
         except:
             return jsonify('Please, fulfill all information(author, language and title)'), 409
+        
+        SQL_QUERY = 'INSERT INTO books (`title`, `author`, `language`) VALUES (\'{}\', \'{}\', \'{}\')'
+            
+        cursor.execute(SQL_QUERY.format(new_title, new_author, new_lang))
+        conn.commit()
+
+        return jsonify({'message': 'The book was created successfully'}), 201
     
 @app.route('/book/<int:id>', methods=['GET', 'PUT', 'DELETE'])
 def single_book(id):
     
     
     if request.method == 'GET':
-        if(len(book_list) > id):
-            return jsonify(book_list[id])
-        else:
-            return "The book does not exists", 404
         
+        SQL_QUERY = 'SELECT * FROM books WHERE id = {}'
         
+        cursor.execute(SQL_QUERY.format(id))
+        
+        book = {}
+        
+        for row in cursor:
+            
+            book = {
+                'id': row[0],
+                'title': row[1],
+                'author': row[2],
+                'language': row [3]
+            }
+            
+        return jsonify(book), 200
         
     if request.method == 'DELETE':
-        if(len(book_list) > id):
-            book_list.pop(id)
         
-            for i in range(len(book_list)):
-                book_list[i]['id'] = i
+        SQL_QUERY = 'DELETE FROM books WHERE id = {}'
+        
+        try:
+            cursor.execute(SQL_QUERY.format(id))
+            conn.commit()
             
-            return {"message": "The book was deleted successfully"}, 200
-        else:
+        except:
             return {"message": "The book does not exists"}, 404
+        
+        return {"message": "The book was deleted successfully"}, 200
         
         
         
     if request.method == 'PUT':
         
-        new_author = book_list[id].get('author')
-        new_lang = book_list[id].get('language')
-        new_title = book_list[id].get('title')
+        book = requests.get('http://localhost:5000/book/{}'.format(id)).json()
         
-        if(len(book_list) > id):        
-            try:
-                new_author = request.form['author']
-            except:
-                pass
+        new_author = book['author']
+        new_title = book['title']
+        new_lang = book['language']
+        
+        
+        try:
+            new_author = request.form['author']
+        except:
+            pass
+        
+        try:
+            new_lang = request.form['language']
+        except:
+            pass
+        
+        try: 
+            new_title = request.form['title']
+        except:
+            pass
             
-            try:
-                new_lang = request.form['language']
-            except:
-                pass
+        SQL_QUERY = 'UPDATE books SET title = \'{}\', author = \'{}\', language = \'{}\' WHERE id = {}'
             
-            try: 
-                new_title = request.form['title']
-            except:
-                pass
+        try:
+            cursor.execute(SQL_QUERY.format(new_title, new_author, new_lang, id))
             
-            new_obj = {
-                'id': id,
-                'author': new_author,
-                'language': new_lang,
-                'title': new_title
-            }
-            
-            book_list[id] = new_obj
-            
-            return jsonify(new_obj)
-            
-        else:
+        except:
             return {"message": "The book does not exists"}, 404
+        
+        conn.commit()
+        return jsonify({"message": "The book was altered successfuly"})
             
     
 if __name__ == '__main__':
